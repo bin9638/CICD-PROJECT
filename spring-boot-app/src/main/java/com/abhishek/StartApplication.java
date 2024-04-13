9bf1a35c9ba17c77450b7d2c83ee5d189f9ce159
bin9638 <vinhkhanhbin@gmail.com>
2024-04-13 23:39:39 +0700

    dsaasds

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven, love you <3");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven, love you <3 <3");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
